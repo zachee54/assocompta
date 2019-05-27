581266d342da7213641dcdb576ab5ae2d9f45a90
Olivier Haas <git.oshaas@dfgh.net>
2019-05-27 14:14:38 +0200

    Install DebugKit

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -66,6 +66,7 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::loadAll(); // Loads all plugins at once
  * CakePlugin::load('DebugKit'); // Loads a single plugin named DebugKit
  */
+CakePlugin::load('DebugKit'); // Loads a single plugin named DebugKit
 
 /**
  * To prefer app translation over plugin translation, you can set
