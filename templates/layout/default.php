1c15bd31021a2f19a4dfd0f671a2f816e0676328
Olivier Haas <git.oshaas@dfgh.net>
2021-05-22 11:16:46 +0200

    Rename templates extension

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -1,57 +1,107 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- * @var \App\View\AppView $this
- */
-
-$cakeDescription = 'CakePHP: the rapid development php framework';
-?>
 <!DOCTYPE html>
 <html>
 <head>
-    <?= $this->Html->charset() ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?= $cakeDescription ?>:
-        <?= $this->fetch('title') ?>
-    </title>
-    <?= $this->Html->meta('icon') ?>
-
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
-
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
-
-    <?= $this->fetch('meta') ?>
-    <?= $this->fetch('css') ?>
-    <?= $this->fetch('script') ?>
+  <?php echo $this->Html->charset(); ?>
+  <meta name="robots" content="noindex, nofollow">
+  <title>CFE</title>
+  <?php
+    echo $this->Html->css('cfe');
+    echo $this->fetch('css');
+    echo $this->fetch('jquery');
+  ?>
+  <link rel="icon" href="<?php echo $this->Html->url('/img/favicon.ico'); ?>"/>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-            <?= $this->Flash->render() ?>
-            <?= $this->fetch('content') ?>
-        </div>
-    </main>
-    <footer>
-    </footer>
+  <header>
+    <div>Centre de Formation et d'Entraide</div>
+    <?php
+    if (AuthComponent::user()) {
+      ?>
+      <aside>
+        <header><?php echo AuthComponent::user('nom'); ?></header>
+        <nav>
+          <ul>
+            <?php
+            if (!AuthComponent::user('readonly')) {
+              ?>
+              <li>
+                <?php
+                echo $this->Html->link(
+                  'Changer mon mot de passe',
+                  array(
+                    'admin' => false,
+                    'controller' => 'users',
+                    'action' => 'moncompte'));
+                ?>
+              </li>
+              <?php
+            }
+            
+            ?>
+            <li>
+              <?php
+              echo $this->Html->link(
+                'Déconnexion',
+                array(
+                  'admin' => false,
+                  'controller' => 'users',
+                  'action' => 'logout'));
+              ?>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <?php
+    }
+    ?>
+  </header>
+  <nav>
+    <ul>
+      <li>
+        <?php
+        echo $this->Html->link('Opérations', array(
+          'admin' => false,
+          'controller' => 'ecritures',
+          'action' => 'index'));
+        ?>
+      </li>
+      <li>
+        <?php
+        echo $this->Html->link('Bilan', array(
+          'admin' => false,
+          'controller' => 'stats',
+          'action' => 'bilan'));
+        ?>
+      </li>
+      <?php
+      if (AuthComponent::user('admin')) {
+        ?>
+        <li>
+          <?php
+          echo $this->Html->link('Administration', array(
+            'admin' => true,
+            'controller' => 'users',
+            'action' => 'index'));
+          ?>
+        </li>
+        <?php
+      }
+      ?>
+    </ul>
+  </nav>
+  <?php
+  
+  echo $this->Flash->render();
+  echo $this->fetch('content');
+  echo $this->fetch('scriptBottom');
+  
+  ?>
+  <footer>
+    <?php
+    echo $this->Html->link('Mentions légales', array(
+      'controller' => 'pages',
+      'action' => 'legal'));
+    ?>
+  </footer>
 </body>
 </html>
