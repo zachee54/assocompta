8a9628650087ec0ad829772d55b0608b6b1aae6e
Olivier Haas <git.oshaas@dfgh.net>
2021-05-23 21:08:48 +0200

    Upgrade moncompte form

--- templates/Users/moncompte.php
+++ templates/Users/moncompte.php
@@ -7,19 +7,17 @@ $this->Html->css(
 <!-- Utiliser la même mise en forme que le formulaire de connexion -->
 <section id="UserLogin">
   <?php
-  echo $this->Form->create(false, array(
-      'inputDefaults' => array(
-        'div' => false,
-        'type' => 'password')));
+  echo $this->Form->create(null);
     
-    echo $this->Form->control('old_password', array(
-      'label' => 'Mot de passe actuel&nbsp;:'));
+    echo $this->Form->label('old_password', 'Mot de passe actuel');
+    echo $this->Form->password('old_password');
     
-    echo $this->Form->control('new_password', array(
-      'label' => 'Nouveau mot de passe&nbsp;:'));
+    echo $this->Form->label('new_password', 'Nouveau mot de passe');
+    echo $this->Form->password('new_password');
     
-    echo $this->Form->control('password_confirm', array(
-      'label' => 'Confirmez le mot de passe&nbsp;:'));
+    echo $this->Form->label('password_confirm',
+      'Confirmez le mot de passe');
+    echo $this->Form->password('password_confirm');
     
     echo $this->Form->submit('Valider', array(
       'class' => 'button'));
